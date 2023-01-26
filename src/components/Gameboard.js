05a469436bb60c117769868f3992ffa7148f9e25
itsechi <wardulinskao@gmail.com>
2023-01-26 14:19:41 +0100

    Add a function to shuffle cards arr

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,20 +1,39 @@
 import { Card } from "./Card";
+import React, { useEffect } from "react";
 
 export const Gameboard = () => {
-  const cards = [];
-  for (let i = 1; i < 6; i++) {
-    const card = {
-      id: i,
-      clicked: false,
-    };
-    cards.push(card);
-  }
+  const [cards, setCards] = React.useState([]);
+  useEffect(() => {
+    setCards(() => {
+      const cards = [];
+      for (let i = 1; i < 6; i++) {
+        const card = {
+          id: i,
+          clicked: false,
+        };
+        cards.push(card);
+      }
+      return cards;
+    });
+  }, []);
 
   const markClicked = (e) => {
     if (!e.target.classList.contains("card")) return;
     cards.map(
       (item) => item.id === +e.target.dataset.id && (item.clicked = true)
     );
+
+    function shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+      return [...array];
+    }
+
+    setCards(shuffleArray(cards));
   };
 
   const generateCards = cards.map((item) => (

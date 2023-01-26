1895fe9211719b022912cdee301147ea3b24b520
itsechi <wardulinskao@gmail.com>
2023-01-26 15:11:55 +0100

    Keep track of score when clicking on cards

--- src/App.js
+++ src/App.js
@@ -5,9 +5,47 @@ import { Score } from "./components/Score";
 function App() {
   const [currentScore, setCurrentScore] = React.useState(0);
 
+  const [cards, setCards] = React.useState([]);
+  React.useEffect(() => {
+    setCards(() => {
+      const cards = [];
+      for (let i = 1; i < 6; i++) {
+        const card = {
+          id: i,
+          clicked: false,
+        };
+        cards.push(card);
+      }
+      return cards;
+    });
+  }, []);
+
+  const markClicked = (e) => {
+    if (!e.target.classList.contains("card")) return;
+    cards.find((item) => item.id === +e.target.dataset.id).clicked
+      ? setCurrentScore(0)
+      : setCurrentScore((prevScore) => prevScore + 1);
+    cards.map(
+      (item) => item.id === +e.target.dataset.id && (item.clicked = true)
+    );
+    console.log(cards.find((item) => item.id === +e.target.dataset.id));
+
+    function shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+      return [...array];
+    }
+
+    setCards(shuffleArray(cards));
+  };
+
   return (
     <>
-      <Gameboard />
+      <Gameboard cards={cards} markClicked={markClicked} />
       <Score currentScore={currentScore} />
     </>
   );
--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,44 +1,10 @@
 import { Card } from "./Card";
-import React, { useEffect } from "react";
+import React from "react";
 
-export const Gameboard = () => {
-  const [cards, setCards] = React.useState([]);
-  useEffect(() => {
-    setCards(() => {
-      const cards = [];
-      for (let i = 1; i < 6; i++) {
-        const card = {
-          id: i,
-          clicked: false,
-        };
-        cards.push(card);
-      }
-      return cards;
-    });
-  }, []);
-
-  const markClicked = (e) => {
-    if (!e.target.classList.contains("card")) return;
-    cards.map(
-      (item) => item.id === +e.target.dataset.id && (item.clicked = true)
-    );
-
-    function shuffleArray(array) {
-      for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-      }
-      return [...array];
-    }
-
-    setCards(shuffleArray(cards));
-  };
-
-  const generateCards = cards.map((item) => (
+export const Gameboard = (props) => {
+  const generateCards = props.cards.map((item) => (
     <Card id={item.id} key={item.id} />
   ));
 
-  return <div onClick={markClicked}>{generateCards}</div>;
+  return <div onClick={props.markClicked}>{generateCards}</div>;
 };

fe1d9a1493ee36fb6f2d5a8b6f732a9ef769df2e
itsechi <wardulinskao@gmail.com>
2023-02-03 14:42:21 +0100

    Add animations to the victory screen

--- src/components/Victory.js
+++ src/components/Victory.js
@@ -2,7 +2,10 @@ export const Victory = (props) => {
   return (
     <div className="overlay">
       <p>VICTORY</p>
-      <button onClick={props.restartGame}>PLAY AGAIN</button>
+      <button onClick={props.restartGame}>
+        <span className="btn__slide"></span>
+        <span className="btn__text">PLAY AGAIN</span>
+      </button>
     </div>
   );
 };
